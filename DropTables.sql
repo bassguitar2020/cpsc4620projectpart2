3c6587d5dda106deb54dfedd612257832bfd3ebd
Benjamin McDonnough <116963084+bmcdonnough@users.noreply.github.com>
2025-11-04 20:20:04 -0500

    Wrote the DropTables.sql file

---
+++ DropTables.sql
@@ -0,0 +1,19 @@
+-- Created by Benjamin McDonnough
+USE PizzaDB;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS pizza_discount;
+DROP TABLE IF EXISTS order_discount;
+DROP TABLE IF EXISTS pizza_topping;
+DROP TABLE IF EXISTS dinein;
+DROP TABLE IF EXISTS pickup;
+DROP TABLE IF EXISTS delivery;
+DROP TABLE IF EXISTS pizza;
+DROP TABLE IF EXISTS ordertable;
+DROP TABLE IF EXISTS discount;
+DROP TABLE IF EXISTS topping;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS basesprice;
+
+SET FOREIGN_KEY_CHECKS = 1;
